4d077c6322791ac99fd70ba62e9f465076a423f4
trikstor <azpk360@yandex.ru>
2017-10-27 10:44:09 +0500

    Начато tdd, созданы и пройдены первые тесты для CircularCloudLayouter

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public CircularCloudLayouter(Point center)
+        {
+            if(center.X < 0 || center.Y < 0)
+                throw new ArgumentException("Coordinates must be positive or zero");
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            return new Rectangle(new Point(10, 10), rectangleSize);
+        }
+    }
+}
---
+++ TagsCloudVisualization/TagsCloudVisualization_Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class TagsCloudVisualization_Tests
+    {
+        [TestCase(-10, 5, "Coordinates must be positive or zero", TestName = "Create a new layout with negative cordinate(s)")]
+        public void CircularCloudLayouter_ThrowException_Constructor(int x, int y, string exMessage)
+        {
+            Action res = () => { new CircularCloudLayouter(new Point(x, y)); };
+            res.ShouldThrow<ArgumentException>().WithMessage(exMessage);
+        }
+
+        [TestCase(15, 5, TestName = "Return rectangle")]
+        public void PutNextRectangle_ReturnRightRectangle(int width, int height)
+        {
+            var layout = new CircularCloudLayouter(new Point(12, 10));
+
+            var actual = layout.PutNextRectangle(new Size(width, height));
+            var expected = new Rectangle(new Point(10, 10), new Size(width, height));
+
+            actual.ShouldBeEquivalentTo(expected, options =>
+                options.ExcludingNestedObjects());
+        }
+
+        /*
+        [TestCase(0, 5, "Size must be positive", 
+            TestName = "Create a new rectangle with negative or zero size")]
+        public void PutNextRectangle_ThrowException(int width, int height, string exMessage)
+        {
+            var layout = new CircularCloudLayouter(new Point(10, 10));
+
+            Action res = () => { layout.PutNextRectangle(new Size(width, height));};
+            res.ShouldThrow<ArgumentException>().WithMessage(exMessage);
+        }
+        */
+    }
+}
